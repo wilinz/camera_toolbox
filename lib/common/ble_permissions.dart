22c81ab2395dbaf85516d09ffd20dd6dc5ba1199
wilinz <weilizan71@gmail.com>
2025-03-14 18:14:53 +0800

    重构权限检查

---
+++ lib/common/ble_permissions.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'logger.dart';
+
+Future<bool> checkBluetoothPermissions() async {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  bool isAndroid = Platform.isAndroid;
+  int sdkVersion = isAndroid ? (await deviceInfo.androidInfo).version.sdkInt : 0;
+
+  // 创建一个空的权限列表并根据设备平台添加权限
+  List<Permission> permissions = [];
+
+  if (isAndroid) {
+    if (sdkVersion < 31) permissions.add(Permission.locationWhenInUse);
+    permissions.addAll([Permission.bluetoothScan, Permission.bluetoothConnect]);
+  } else {
+    permissions.addAll([Permission.bluetoothScan, Permission.bluetoothConnect]);
+  }
+
+  // 请求权限并判断是否全部授权
+  Map<Permission, PermissionStatus> statuses = await permissions.request();
+  bool allGranted = statuses.values.every((status) => status.isGranted);
+
+  if (allGranted && (isAndroid && sdkVersion >= 31 || statuses[Permission.locationWhenInUse]?.isGranted == true)) {
+    logger.i("权限检查完成");
+    return true;
+  }
+
+  logger.i("位置或蓝牙权限不足，请检查设置！");
+  return false;
+}
--- lib/modules/blescan/blesacn_controller.dart
+++ lib/modules/blescan/blesacn_controller.dart
@@ -4,10 +4,10 @@ import 'dart:io';
 import 'package:camera_toolbox/common/logger.dart';
 import 'package:camera_toolbox/toast.dart';
 import 'package:collection/collection.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
+
+import '../../common/ble_permissions.dart';
 
 class BLEScanController extends GetxController {
   final isScanning = false.obs;
@@ -17,39 +17,14 @@ class BLEScanController extends GetxController {
 
   StreamSubscription<List<ScanResult>>? _scanSubscription;
 
-  Future<bool> _checkPermissions() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    bool isAndroid = Platform.isAndroid;
-    int sdkVersion = isAndroid ? (await deviceInfo.androidInfo).version.sdkInt : 0;
-
-    // 创建一个空的权限列表并根据设备平台添加权限
-    List<Permission> permissions = [];
-
-    if (isAndroid) {
-      if (sdkVersion < 31) permissions.add(Permission.locationWhenInUse);
-      permissions.addAll([Permission.bluetoothScan, Permission.bluetoothConnect]);
-    } else {
-      permissions.addAll([Permission.bluetoothScan, Permission.bluetoothConnect]);
-    }
-
-    // 请求权限并判断是否全部授权
-    Map<Permission, PermissionStatus> statuses = await permissions.request();
-    bool allGranted = statuses.values.every((status) => status.isGranted);
-
-    if (allGranted && (isAndroid && sdkVersion >= 31 || statuses[Permission.locationWhenInUse]?.isGranted == true)) {
-      logger.i("权限检查完成");
-      return true;
-    }
-
-    toastFailure0("位置或蓝牙权限不足，请检查设置！");
-    return false;
-  }
-
   Future<void> startScan() async {
     toast("开始扫描设备...");
 
-    final hasPermissions = await _checkPermissions();
-    if (!hasPermissions) return;
+    final hasPermissions = await checkBluetoothPermissions();
+    if (!hasPermissions) {
+      toastFailure0("请授予蓝牙权限");
+      return;
+    }
 
     // 检查蓝牙是否开启
     if (await FlutterBluePlus.adapterState.first != BluetoothAdapterState.on) {
